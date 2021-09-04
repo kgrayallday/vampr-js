17c4a960900107a3acba52dbec9bf5f58e54c9f7
kyl graym <kylemaxgray@gmail.com>
2021-09-04 23:43:24 +0000

    vampire fun

--- vampire.js
+++ vampire.js
@@ -10,22 +10,41 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let relative = this.creator;
+    let count = 0;
 
+    while (relative) {
+      relative = relative.creator;
+      count++
+    }
+    return count
   }
+
+  // Returns array of ancestors
+  // get bloodLine() {
+  //   const ancestors = [this];
+  //   let nextInLine = this.creator;
+  //   while (nextInLine){
+  //     ancestors.push(this.creator);
+  //     nextInLine = nextInLine.creator;
+  //   }
+  //   return ancestors;
+  // }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,6 +55,9 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    // console.log('bloodline', this.bloodLine);
+    // return this.bloodLine;
+    
 
   }
 }
